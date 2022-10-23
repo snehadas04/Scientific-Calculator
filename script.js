c4e6d3a776ce247a93b9759cb888b51bd5271dab
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2022-10-23 13:52:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const display = document.querySelector("#display");
+const calcul = document.querySelectorAll("button");
+
+function sin() {
+  document.calcul.result.value = Math.sin(document.calcul.result.value);
+}
+
+function cos() {
+  document.calcul.result.value = Math.cos(document.calcul.result.value);
+}
+
+function tan() {
+  document.calcul.result.value = Math.tan(document.calcul.result.value);
+}
+
+function BACKSPC() {
+  var a = document.calcul.result.value;
+  document.calcul.result.value = a.substr(0, a.length - 1);
+}
+
+function square() {
+  document.calcul.result.value = Math.pow(document.calcul.result.value, 2);
+}
+
+function cubed() {
+  document.calcul.result.value = Math.pow(document.calcul.result.value, 3);
+}
+
+function sqrt2() {
+  document.calcul.result.value = Math.pow(document.calcul.result.value, 1 / 2);
+}
+
+function sqrt3() {
+  document.calcul.result.value = Math.pow(document.calcul.result.value, 1 / 3);
+}
+
+function number(value) {
+  document.calcul.result.value += value;
+}
+
+function remv() {
+  document.calcul.result.value = " ";
+}
+
+function equal() {
+  document.calcul.result.value = eval(document.calcul.result.value);
+}
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
